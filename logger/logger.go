949ef376decf6f2312ad8c354a74824ba2b61b05
Peter Braun <pbraun@redhat.com>
2017-03-12 22:42:27 +0100

    make scm-go work with current config format

--- logger/logger.go
+++ logger/logger.go
@@ -5,20 +5,43 @@ import (
 
 	"github.com/maleck13/scm-go/Godeps/_workspace/src/github.com/Sirupsen/logrus"
 	"github.com/maleck13/scm-go/Godeps/_workspace/src/github.com/fheng/scm-go/config"
+	"io"
 )
 
-//TODO write to file and stdout
-
 var Logger = logrus.New()
+var logFilePath = "/var/log/feedhenry/fh-scm/"
 
-func InitLogger(loggers []config.Logger) *logrus.Logger {
-	logger := loggers[0]
+func directoryExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+
+	return false, err
+}
+
+func InitLogger(loggers config.Logger) *logrus.Logger {
+	logger := loggers.Streams[0]
 	Logger.Formatter = new(logrus.JSONFormatter)
-	f, err := os.OpenFile(logger.Filename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	// Check if we can use the standard log directory. If not
+	// fall back to tmp
+	if result, _ := directoryExists(logFilePath); !result {
+		logFilePath = "/tmp/"
+	}
+
+	// Use the 'Stream' value from the fh-scm config as the filename
+	// for now
+	logFileName := logFilePath + logger.Stream
+
+	f, err := os.OpenFile(logFileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		Logger.Fatal(err)
 	}
-	Logger.Out = f
+
+	// Log to file and stdout
+	Logger.Out = io.MultiWriter(f, os.Stdout)
+
 	switch logger.Level {
 	case "info":
 		Logger.Level = logrus.InfoLevel
